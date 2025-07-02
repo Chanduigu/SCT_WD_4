734019459368507efada3d5159d77dd7b03521d0
Chanduigu <hhc90809@gmail.com>
2025-07-02 10:34:11 +0530

    Initial commit: FocusFlow To-Do App with Firebase

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDfuOrLVQBsatKDAk1jru5Ok0rH42RsHqI",
+  authDomain: "to-do-c2c21.firebaseapp.com",
+  databaseURL: "https://to-do-c2c21-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "to-do-c2c21",
+  storageBucket: "to-do-c2c21.appspot.com",
+  messagingSenderId: "948561635865",
+  appId: "1:948561635865:web:d563fda0f4c297212d5888"
+};
+firebase.initializeApp(firebaseConfig);
